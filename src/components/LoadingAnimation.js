dfa1277528aaa75ca0834f7ce761cd8e11f3db0a
kiarashAlizadeh <97706913+kiarashAlizadeh@users.noreply.github.com>
2023-10-22 18:24:59 +0330

    loading animation added but it has bugs

---
+++ src/components/LoadingAnimation.js
@@ -0,0 +1,38 @@
+import "../assets/js/animation-loading.js"
+function LoadinAnimation() {
+  return (
+    <div
+      id="loading-screen"
+      className="fixed top-0 z-50 flex min-h-screen w-full items-center justify-center bg-white"
+    >
+      <div className="boxes">
+        <div className="box">
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+        </div>
+        <div className="box">
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+        </div>
+        <div className="box">
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+        </div>
+        <div className="box">
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default LoadinAnimation
